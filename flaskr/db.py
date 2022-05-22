b798b89531181efb31e84b51aa39216d61b55f9f
Zen <ztang230@gmail.com>
2022-05-22 14:50:53 -0500

    flask_migrate - init

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import joinedload
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-
+migrate = Migrate()
 
 def get_db():
     if "db" not in g:
@@ -34,6 +35,6 @@ def init_db_command():
 def init_app(app):
     """Adds db methods to current app"""
     db.init_app(app)
+    migrate.init_app(app, db)
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
-
