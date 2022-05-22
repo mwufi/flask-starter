c12bef5ac810f77ceff36345127ccbfe3f84194f
Zen <ztang230@gmail.com>
2022-05-22 15:00:21 -0500

    header links

--- flaskr/tokens.py
+++ flaskr/tokens.py
@@ -1,13 +1,12 @@
 from flaskr.auth import login_required
-from flask import (
-    Blueprint, g, render_template, redirect, url_for, request
-)
+from flask import Blueprint, g, render_template, redirect, url_for, request
 from flaskr.models import Token
 
-bp = Blueprint('tokens', __name__, url_prefix='/tokens')
+bp = Blueprint("tokens", __name__, url_prefix="/tokens")
+
 
 @bp.route("/")
 @login_required
-def tokens():
+def index():
     tokens = Token.query.filter(Token.user_id == g.user.id).all()
-    return render_template('tokens/index.html', tokens=tokens)
+    return render_template("tokens/index.html", tokens=tokens)
